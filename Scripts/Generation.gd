2257eb79b893c05725045b068d8213a0da42d4ed
aarongerbrandt <aarond.gerbrandt@gmail.com>
2023-05-04 18:42:42 -0500

    Added rooms that will collide with each other

--- Scripts/Generation.gd
+++ Scripts/Generation.gd
@@ -1,11 +1,15 @@
 extends Spatial
 
-export(int, 1, 50) var number_of_rooms = 25
-export(float, 50, 100) var room_placement_radius = 200
+export(float) var time_to_room_stop = 10
+export(int) var number_of_rooms = 25
+export(float) var room_placement_radius = 200
 export(int, 1, 10) var min_room_size = 5
 export(int, 15, 25) var max_room_size = 20
 
+const Room1 = preload("res://Rooms/Room1.tscn")
+
 const BEGINNING_HEIGHT = 5
+const TILE_SIZE = 4
 
 func _ready():
 	generate()
@@ -15,6 +19,12 @@ func generate():
 	for i in range(number_of_rooms):
 		var room_position = getRandom3DPointInCircle(room_placement_radius)
 		place_room(room_position)
+	
+	yield(get_tree().create_timer(time_to_room_stop), "timeout")
+	_stop_rooms()
+
+func _round(n: float, m: int) -> int:
+	return int((n + m - 1) / m) * m
 
 func getRandom3DPointInCircle(radius: float) -> Vector3:
 	# Uses algorithm from 
@@ -28,8 +38,8 @@ func getRandom3DPointInCircle(radius: float) -> Vector3:
 	else:
 		r = u
 	
-	var x = radius * r * cos(t)
-	var z = radius * r * sin(t)
+	var x = _round(radius * r * cos(t), TILE_SIZE)
+	var z = _round(radius * r * sin(t), TILE_SIZE)
 	
 	return Vector3(x, BEGINNING_HEIGHT, z)
 
@@ -40,7 +50,11 @@ func place_room(room_position: Vector3) -> void:
 		rand_range(min_room_size, max_room_size)
 	)
 	
-	var cube = CSGBox.new()
-	cube.global_translation = room_position
-	cube.scale = room_dimensions
-	add_child(cube)
+	var room = Room1.instance()
+	room.load_position(room_position, room_dimensions)
+	$Rooms.add_child(room)
+
+func _stop_rooms():
+	print("Stopping Rooms")
+	for room in $Rooms.get_children():
+		room.mode = RigidBody.MODE_STATIC
