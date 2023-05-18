aa37769cf0a41ce8b757f411d649d94e3e3744ef
aarongerbrandt <aarond.gerbrandt@gmail.com>
2023-05-18 11:15:14 -0500

    Improved player movement

--- Resources/Entities/Player.gd
+++ Resources/Entities/Player.gd
@@ -1,9 +1,10 @@
 extends KinematicBody
 
-export var speed = 100
+export var base_speed = 100
 export var acceleration = 75
-export var gravity = 100
-export var jump_speed = 75
+export var gravity = 400
+export var jump_speed = 150
+export var sprint_modifier = 1.7
 
 export var sensitivity = .2
 
@@ -24,10 +25,7 @@ func _physics_process(delta):
 	head.rotation_degrees.x = look_rot.x
 	rotation_degrees.y = look_rot.y
 	
-	if !is_on_floor():
-		velocity.y -= gravity * delta
-	elif Input.is_action_just_pressed("Jump"):
-		velocity.y = jump_speed
+	velocity.y -= gravity * delta
 	
 	move_dir = Vector3(
 		Input.get_action_strength("Right") - Input.get_action_strength("Left"),
@@ -35,10 +33,15 @@ func _physics_process(delta):
 		Input.get_action_strength("Backward") - Input.get_action_strength("Forward")
 	).normalized().rotated(Vector3.UP, rotation.y)
 	
+	var speed = base_speed * sprint_modifier if Input.is_key_pressed(KEY_SHIFT) else base_speed
+	
 	velocity.x = lerp(velocity.x, move_dir.x * speed, acceleration * delta)
 	velocity.z = lerp(velocity.z, move_dir.z * speed, acceleration * delta)
 	
 	velocity = move_and_slide(velocity, Vector3.UP)
+	
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = jump_speed
 
 func _input(event):
 	if event is InputEventMouseMotion:
